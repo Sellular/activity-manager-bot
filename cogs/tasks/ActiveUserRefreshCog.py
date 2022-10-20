69c62027665cd2b079a6480177ee0eac380a34dc
Sellular <sellular13@gmail.com>
2022-10-20 21:29:55 +0000

    Fixed role removal

--- cogs/tasks/ActiveUserRefreshCog.py
+++ cogs/tasks/ActiveUserRefreshCog.py
@@ -18,41 +18,39 @@ class ActiveUserRefreshCog(commands.Cog):
     @tasks.loop(hours=1)
     async def activeUserRefresh(self):
         bot = self.bot
-        try:
-            guildConfig = GeneralUtils.getConfig('guild')
-            inactiveRoleID = guildConfig['inactive_role_id']
-            guildID = guildConfig['guild_id']
+        guildConfig = GeneralUtils.getConfig('guild')
+        inactiveRoleID = guildConfig['inactive_role_id']
+        guildID = guildConfig['guild_id']
 
-            guild = discord.utils.get(bot.guilds, id=int(guildID))
-            inactiveRole = discord.utils.get(
-                guild.roles, id=int(inactiveRoleID))
+        guild = discord.utils.get(bot.guilds, id=int(guildID))
+        inactiveRole = discord.utils.get(
+            guild.roles, id=int(inactiveRoleID))
 
-            ignoredRoles = IgnoredRoleDAO.getIgnoredRoles()
-            ignoredMembers = [member.id for member in [mem for mem in [
-                discord.utils.get(guild.roles, id=roleID).members for roleID in ignoredRoles]]]
+        ignoredRoles = IgnoredRoleDAO.getIgnoredRoles()
+        ignoredMembers = [member.id for member in [mem for mem in [
+            discord.utils.get(guild.roles, id=roleID).members for roleID in ignoredRoles]]]
 
-            activity_timestamp_ids = [
-                (memberIDString, bot.activeUsersCache[f'{memberIDString}']) for memberIDString in bot.activeUsersCache.keys()]
-            UserActivityDAO.upsertMany(activity_timestamp_ids)
+        activity_timestamp_ids = [
+            (memberIDString, bot.activeUsersCache[f'{memberIDString}']) for memberIDString in bot.activeUsersCache.keys()]
+        UserActivityDAO.upsertMany(activity_timestamp_ids)
 
-            activeActivities = UserActivityDAO.getUserActivityByActive(True)
-            now = datetime.now(timezone.utc)
-            twoWeeksAgo = now - timedelta(days=14)
-            inactive_id_list = []
-            for activity in activeActivities:
-                if activity.activeTimestamp < twoWeeksAgo:
-                    member = discord.utils.get(
-                        guild.members, id=int(activity.memberID))
-                    if not member.bot and member.id not in ignoredMembers:
-                        await member.remove_roles(member.roles)
-                        await member.add_roles(inactiveRole)
-                        inactive_id_list.append((activity.memberID,))
-                        bot.activeUsersCache.pop(activity.memberID)
+        activeActivities = UserActivityDAO.getUserActivityByActive(True)
+        now = datetime.now(timezone.utc)
+        twoWeeksAgo = now - timedelta(days=14)
+        inactive_id_list = []
+        for activity in activeActivities:
+            if activity.activeTimestamp < twoWeeksAgo:
+                member = discord.utils.get(
+                    guild.members, id=int(activity.memberID))
+                if not member.bot and member.id not in ignoredMembers:
+                    await member.edit(roles=[])
+                    await member.add_roles(inactiveRole)
+                    inactive_id_list.append((activity.memberID,))
+                    bot.activeUsersCache.pop(activity.memberID)
 
-            if inactive_id_list:
-                UserActivityDAO.userActivitySetManyInactive(inactive_id_list)
-        except Exception as error:
-            print(error)
+        if inactive_id_list:
+            UserActivityDAO.userActivitySetManyInactive(inactive_id_list)
+        
 
     @activeUserRefresh.before_loop
     async def before_userdump(self):
