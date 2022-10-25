2bed120640b597f78c9456bd89eb9c6bcba59a69
Sellular <sellular13@gmail.com>
2022-10-25 02:53:04 +0000

    Add optional additional roles to ignore

--- cogs/commands/ActivityIgnoreRoleCog.py
+++ cogs/commands/ActivityIgnoreRoleCog.py
@@ -10,14 +10,19 @@ class ActivityIgnoreRoleCog(commands.Cog):
         self.bot = bot
 
     @slash_command(name='activityignorerole', guild_ids=[int(GeneralUtils.getConfig('guild')['guild_id'])], description='Ignore activity of given role')
-    @option('role', Role, description='Role to be ignored', required=True)
+    @option('role0', Role, description='Role to be ignored', required=True)
+    @option('role1', Role, description="Next role to be ignored", required=False)
+    @option('role2', Role, description="Third role to be ignored", required=False)
     @commands.has_permissions(administrator=True)
-    async def activityIgnoreRole(self, interaction: Interaction, role: Role):
-        if role.id in self.bot.ignoredRoles:
-            await interaction.response.send_message("Role is already ignored.", ephemeral=True)
-        else:
-            IgnoredRoleDAO.insert(role.id)
-            await interaction.response.send_message("Role has been ignored.", ephemeral=True)
+    async def activityIgnoreRole(self, interaction: Interaction, role0: Role, role1: Role, role2: Role):
+        roles = [role0, role1, role2]
+        for role in roles:
+            if role:
+                if role.id in self.bot.ignoredRoles:
+                    await interaction.response.send_message(f"Role {role.name} is already ignored.", ephemeral=True)
+                else:
+                    IgnoredRoleDAO.insert(role.id)
+                    await interaction.response.send_message(f"Role {role.name} has been ignored.", ephemeral=True)
 
 
 def setup(bot: commands.Bot):
